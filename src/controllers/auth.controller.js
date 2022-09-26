88c2f7c611bfcedcd7f4d3220d8ac0662b51b75a
Yousef Ashraf <bosayousef981@gmail.com>
2022-09-26 20:28:49 +0200

    Finalize login exercise

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,4 +3,5 @@ import * as AuthService from '../services/auth/index.js';
 const router = Router();
 
 router.post('/register', AuthService.register);
+router.post('/login', AuthService.login);
 export default router;
