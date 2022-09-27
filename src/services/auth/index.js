b23e61407a3c8bb7a747a2a1c7ec84da349dbcd2
mohamed tarek <mt1348@fayoum.edu.eg>
2022-09-27 19:27:16 +0200

    pretified

--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -1,3 +1,2 @@
 export * from './register.js';
 export * from './login.js';
-
--- src/services/books/deleteBook.js
+++ src/services/books/deleteBook.js
@@ -2,16 +2,16 @@ import Books from '../../helpers/db/books.db.js';
 import { badRequestResponse } from '../../helpers/functions/ResponseHandler.js';
 import { okResponse } from './../../helpers/functions/ResponseHandler.js';
 
-export function deleteBook(req, res,next) {
-	try{
-	const { id } = req.params;
-	const bookIndex = Books.findIndex((book) => book.id === parseInt(id));
-	if (bookIndex === -1) {
-		return badRequestResponse(res,'Book not found')
+export function deleteBook(req, res, next) {
+	try {
+		const { id } = req.params;
+		const bookIndex = Books.findIndex((book) => book.id === parseInt(id));
+		if (bookIndex === -1) {
+			return badRequestResponse(res, 'Book not found');
+		}
+		Books.splice(bookIndex, 1);
+		return okResponse(res, 'Book deleted', Books);
+	} catch (err) {
+		next(err);
 	}
-	Books.splice(bookIndex, 1);
-	return okResponse(res,'Book deleted',Books)
-}catch(err){
-	next(err)
-}
 }
